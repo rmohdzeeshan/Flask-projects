5ac44cc1ef12c1914fa9429622a6baa50953b4bc
rmohdzeeshan <111166942+rmohdzeeshan@users.noreply.github.com>
2022-09-22 10:21:01 +0530

    Add files via upload

---
+++ dao/e_verify.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+def check_user():
+    # check if user already exists
+    # if exists send warning
+    # send signup success
+    # Take latest record 10, +1
+
+    pass
+
---
+++ service/e_log.py
@@ -0,0 +1,9 @@
+
+
+
+
+
+def check_user():
+    # load data and check user exists or not
+    # pass data to dao layer
+    pass
